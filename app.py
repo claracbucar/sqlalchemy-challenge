fd9b6376ee29d2bf9c7c8948515cb208a356a3de
claracbucar <114194355+claracbucar@users.noreply.github.com>
2023-03-08 13:48:30 -0500

    resubmission

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import sqlalchemy
 import numpy as np
 import pandas as pd
@@ -8,7 +8,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 
 
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite",connect_args={'check_same_thread': False})
+
 Base = automap_base()
 Base.prepare(autoload_with=engine)
 
@@ -24,7 +25,9 @@ def welcome():
     """Lists all available api routes"""
     return (
         f"Available routes:<br/>"
-        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
     )
 
 
@@ -34,26 +37,28 @@ def precipitation():
     yearago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     dateandprcp = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= yearago).all()
+    
+    result = {date:prcp for date,prcp in dateandprcp}
 
-    return jsonify(dateandprcp)
+    return jsonify(result)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
     """Returns a JSON list of stations from the dataset."""
-    totalstationsnames = session.query(Station)
-    for stationname in totalstationsnames:
-        print(stationname.station)
-    return jsonify(totalstationsnames)
+    totalstationsnames = session.query(Station.station).all()
+    result = list(np.ravel(totalstationsnames))
+    return jsonify(station = result)
 
 @app.route("/api/v1.0/tobs")
 def temperatures():
     """Returns a JSON list of temperature observations for the previous year"""
+    yearago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     activestationlastyear = session.query(Measurement.tobs, Measurement.date).\
         filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date >= yearago).all()
-    print(activestationlastyear)
-    return jsonify(activestationlastyear)
+    result = list(np.ravel(activestationlastyear))
+    return jsonify(result = result)
 
 
 session.close()
